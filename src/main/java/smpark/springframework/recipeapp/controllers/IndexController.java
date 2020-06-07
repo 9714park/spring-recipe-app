8e1e0b21a49cbc6b8a669434de72335eb040a47b
Sang Min Park <9714park@gmail.com>
2020-06-07 12:27:04 -0400

    Add bootstrap to initialize recipe data.

--- src/main/java/smpark/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/smpark/springframework/recipeapp/controllers/IndexController.java
@@ -2,11 +2,30 @@ package smpark.springframework.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import smpark.springframework.recipeapp.domain.Category;
+import smpark.springframework.recipeapp.domain.UnitOfMeasure;
+import smpark.springframework.recipeapp.repositories.CategoryRepository;
+import smpark.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat ID is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
