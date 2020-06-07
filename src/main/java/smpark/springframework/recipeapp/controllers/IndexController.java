cf4747015e88dfc0b5e28ae931d5783af59e5007
Sang Min Park <9714park@gmail.com>
2020-06-07 14:04:36 -0400

    Update main page to display list of recipes.

--- src/main/java/smpark/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/smpark/springframework/recipeapp/controllers/IndexController.java
@@ -1,31 +1,29 @@
 package smpark.springframework.recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import smpark.springframework.recipeapp.domain.Category;
 import smpark.springframework.recipeapp.domain.UnitOfMeasure;
 import smpark.springframework.recipeapp.repositories.CategoryRepository;
 import smpark.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import smpark.springframework.recipeapp.services.RecipeService;
 
 import java.util.Optional;
 
 @Controller
 public class IndexController {
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+  private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+    public String getIndexPage(Model model){
+       model.addAttribute("recipes", recipeService.getRecipes());
 
-        System.out.println("Cat ID is: " + categoryOptional.get().getId());
-        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
